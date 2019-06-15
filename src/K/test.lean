2a6049895b000ecb4cbb5cc1ed1d8090cceed651
minchaowu <logic.mcwu@gmail.com>
2019-06-16 02:14:03 +1000

    add abbreviations of iff

--- src/K/test.lean
+++ src/K/test.lean
@@ -1,14 +1,25 @@
 import .tableau
 open fml
 
+-- A simple workaround for fml.iff.
+-- Unlike fml.impl, this is really a trusted string-level translation 
+-- similar to a bash script modifiying the benchmark text.
+-- Only k_poly_p and k_poly_n contain iffs.
+
+def fml.iff (φ ψ : fml) := and (impl φ ψ) (impl ψ φ) 
+
 def test (ϕ : fml) := fml_is_sat [neg ϕ]
 
 local infix ` ∨ `:1000 := fml.or
 local infix ` ∧ `:1000 := fml.and
 local infix ` -> `:1000 := fml.impl
+local infix ` <-> `:1000 := fml.iff
+
 local prefix `~` := fml.neg
 
 precedence `p`:max
 local prefix ` p ` := fml.var
 
 set_option profiler true
+
+-- #eval test ()
--- src/S4/test.lean
+++ src/S4/test.lean
@@ -1,14 +1,19 @@
 import .tableau
 open fml
 
+def fml.iff (φ ψ : fml) := and (impl φ ψ) (impl ψ φ) 
+
 def test (ϕ : fml) := fml_is_sat [neg ϕ]
 
 local infix ` ∨ `:1000 := fml.or
 local infix ` ∧ `:1000 := fml.and
 local infix ` -> `:1000 := fml.impl
+local infix ` <-> `:1000 := fml.iff
 local prefix `~` := fml.neg
 
 precedence `p`:max
 local prefix ` p ` := fml.var
 
 set_option profiler true
+
+-- #eval test ()
